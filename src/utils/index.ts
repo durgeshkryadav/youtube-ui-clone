b3bb3e56767517c50bb328ac82fe7dcd6e89a88f
Durgesh Yadav <57389386+durgeshkryadav@users.noreply.github.com>
2024-04-13 23:30:42 +0530

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { parseVideoDuration } from "./parseVideoDuration";
 export { timeSince } from "./timeSince";
 export { parseData } from "./parseData";
 export { parseRecommendedData } from "./parseRecommendedData";
+// REACT_APP_YOUTUBE_DATA_API_KEY = AIzaSyB1kv_18HdEwUN7NvlFKBnKYX1e5b2Mgm4
